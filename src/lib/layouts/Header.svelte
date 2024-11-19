a7c404dbd46eb2abd2401f549c2cee6c778638d7
Aaron Jun <aaronsaejun@gmail.com>
2024-11-19 11:51:47 -0800

    Update Header.svelte

--- src/lib/layouts/Header.svelte
+++ src/lib/layouts/Header.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Button from '$lib/ui/button/Button.svelte';
+	import Button from '$lib/ui/button/button.svelte';
 	import { AlignJustify, XIcon, Sun, Moon } from 'lucide-svelte';
 	import { fly } from 'svelte/transition';
 	import PIQLogo from '$lib/assets/imgs/PIQLogo_Orange.svg';
